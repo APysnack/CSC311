ed8d6818b03d2385fa155df19568b2b6e00f9fce
Alan Pysnack <Apysnack@gmail.com>
2020-01-26 12:54:16 -0600

    Server and Client both version 1

--- Client.py
+++ Client.py
@@ -17,6 +17,7 @@ username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
 
 while True:
+
     message = input(f"{my_username} > ")
     if message:
         message = message.encode('utf-8')
@@ -26,19 +27,19 @@ while True:
     try:
         while True:
         # receive things
-        username_header = client_socket.recv(HEADER_LENGTH)
-        if not len(username_header):
-            print("connection closed by the server")
-            sys.exit()
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("connection closed by the server")
+                sys.exit()
 
-        username_length = int(username_length.decode('utf-8').strip())
-        username = client_socket.recv(username_length).decode('utf-8')
+            username_length = int(username_header.decode('utf-8').strip())
+            username = client_socket.recv(username_length).decode('utf-8')
 
-        message_header = client_socket.recv(HEADER_LENGTH)
-        message_length = int(message_header.decode('utf-8').strip())
-        message = client_socket.recv(message_length).decode('utf-8')
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
 
-        print(f"{username} > {message}")
+            print(f"{username} > {message}")
 
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
